3593f5b8d298d29eb0af990e55271bc6ce861679
Sebastian Sokolowski <sebo366@gmail.com>
2017-09-13 22:16:26 +0200

    Add gradle and spring boot.

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Sebastian Sokolowski on 13.09.17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
