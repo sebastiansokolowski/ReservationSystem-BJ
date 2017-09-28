729f4095d757cc170db0b4b0031ddadba7607632
Sebastian Sokolowski <sebo366@gmail.com>
2017-09-28 02:59:28 +0200

    Redirect to main page after success login

--- src/main/java/bj/pranie/config/WebSecurityConfig.java
+++ src/main/java/bj/pranie/config/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/week")
                 .permitAll()
                 .and()
                 .logout()
