9ee0a3e266e898acc6c08188e022997c33f4602f
Sebastian Sokolowski <sebo366@gmail.com>
2017-10-01 16:13:23 +0200

    Implement user settings page and logic.

--- src/main/java/bj/pranie/controller/UserController.java
+++ src/main/java/bj/pranie/controller/UserController.java
@@ -5,8 +5,10 @@ import bj.pranie.dao.UserDao;
 import bj.pranie.entity.Room;
 import bj.pranie.entity.User;
 import bj.pranie.model.UserRegistrationModel;
+import bj.pranie.model.UserSettingsModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,33 +32,93 @@ public class UserController {
     @Autowired
     private RoomDao roomDao;
 
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    public String userSettings(Model model) {
+        model.addAttribute("userSettingsModel", new UserSettingsModel());
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return "user/settings";
+    }
+
+    @RequestMapping(value = "/settings", method = RequestMethod.POST)
+    public ModelAndView saveUserSettings(@ModelAttribute("userSettingsModel") @Valid UserSettingsModel userSettingsModel, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!userSettingsModel.getPassword().equals(user.getPassword())) {
+            bindingResult.rejectValue("password", "error.userRegistrationModel", "Podane hasło jest nieprawidłowe.");
+        }
+
+        User userExist;
+        if (userSettingsModel.isSetNewUsername()) {
+            userExist = userDao.findByUsername(userSettingsModel.getNewUsername());
+            if (userExist != null) {
+                bindingResult.rejectValue("newUsername", "error.userSettingsModel", "Podana nazwa użytkownika istnieje już w bazie.");
+            }
+        }
+        if (userSettingsModel.isSetNewEmail()) {
+            userExist = userDao.findByEmail(userSettingsModel.getNewEmail());
+            if (userExist != null) {
+                bindingResult.rejectValue("newEmail", "error.userSettingsModel", "Podany adres email istnieje już w bazie.");
+            }
+        }
+        if (userSettingsModel.isSetNewPassword()) {
+            if (!userSettingsModel.getNewPassword().equals(userSettingsModel.getNewPasswordRepeat())) {
+                bindingResult.rejectValue("newPasswordRepeat", "error.userRegistrationModel", "Powtórzone hasło jest różne od wpisanego.");
+            }
+        }
+
+        if (!bindingResult.hasErrors()) {
+            if (userSettingsModel.isSetNewUsername()) {
+                user.setUsername(userSettingsModel.getNewUsername());
+            }
+            if (userSettingsModel.isSetNewEmail()) {
+                user.setEmail(userSettingsModel.getNewEmail());
+            }
+            if (userSettingsModel.isSetNewName()) {
+                user.setName(userSettingsModel.getNewName());
+            }
+            if (userSettingsModel.isSetNewPassword()) {
+                user.setPassword(userSettingsModel.getNewPassword());
+            }
+
+            userDao.save(user);
+
+            modelAndView.addObject("successMessage", "Zmiany zostały zachowane pomyślnie.");
+        }
+
+        modelAndView.addObject("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        modelAndView.setViewName("user/settings");
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registrationForm(Model model) {
-        model.addAttribute("user", new UserRegistrationModel());
+        model.addAttribute("userRegistrationModel", new UserRegistrationModel());
         model.addAttribute("rooms", roomDao.findAll());
         return "user/registration";
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public ModelAndView createUser(@ModelAttribute("user") @Valid UserRegistrationModel userRegistrationModel, BindingResult bindingResult) {
+    public ModelAndView createUser(@ModelAttribute("userRegistrationModel") @Valid UserRegistrationModel userRegistrationModel, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
 
         User userExist = userDao.findByEmail(userRegistrationModel.getEmail());
         if (userExist != null) {
-            bindingResult.rejectValue("email", "error.user", "Podany adres email istnieje już w bazie.");
+            bindingResult.rejectValue("email", "error.userRegistrationModel", "Podany adres email istnieje już w bazie.");
         }
         userExist = userDao.findByUsername(userRegistrationModel.getUsername());
         if (userExist != null) {
-            bindingResult.rejectValue("username", "error.user", "Podana nazwa użytkownika istnieje już w bazie.");
+            bindingResult.rejectValue("username", "error.userRegistrationModel", "Podana nazwa użytkownika istnieje już w bazie.");
         }
         if (!userRegistrationModel.getPassword().equals(userRegistrationModel.getPasswordRepeat())) {
-            bindingResult.rejectValue("passwordRepeat", "error.user", "Powtórzone hasło jest różne od wpisanego.");
+            bindingResult.rejectValue("passwordRepeat", "error.userRegistrationModel", "Powtórzone hasło jest różne od wpisanego.");
         }
         Room room = roomDao.findOne(userRegistrationModel.getRoomId());
         if (room == null) {
-            bindingResult.rejectValue("room", "error.user", "Wybierz pokój z listy.");
+            bindingResult.rejectValue("room", "error.userRegistrationModel", "Wybierz pokój z listy.");
         } else if (userDao.findByRoom(room).size() >= room.getPeoples()) {
-            bindingResult.rejectValue("room", "error.user", "Brak miejsca w wybranym pokoju.");
+            bindingResult.rejectValue("room", "error.userRegistrationModel", "Brak miejsca w wybranym pokoju.");
         }
 
         if (!bindingResult.hasErrors()) {
---
+++ src/main/java/bj/pranie/model/UserSettingsModel.java
@@ -0,0 +1,121 @@
+package bj.pranie.model;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Sebastian Sokolowski on 01.10.17.
+ */
+public class UserSettingsModel {
+
+    @NotNull
+    private boolean setNewUsername;
+
+    @Length(min = 3, max = 30, message = "Nazwa użytkownika musi być dłuższe niż 3 znaki i krótsza niż 30.")
+    private String newUsername;
+
+    @NotNull
+    private boolean setNewName;
+
+    @Length(min = 3, max = 30, message = "Imię musi być dłuższe niż 3 znaki i krótsze niż 30.")
+    private String newName;
+
+    @NotNull
+    private boolean setNewEmail;
+
+    @Email(message = "Błędny adres email.")
+    private String newEmail;
+
+    @NotNull
+    private boolean setNewPassword;
+
+    @Length(min = 5, max = 30, message = "Hasło musi być dłuższe niż 3 znaki i krótsze niż 30.")
+    private String newPassword;
+
+    private String newPasswordRepeat;
+
+    @NotNull(message = "Proszę podać aktualnie używane hasło")
+    private String password;
+
+    public boolean isSetNewUsername() {
+        return setNewUsername;
+    }
+
+    public void setSetNewUsername(boolean setNewUsername) {
+        this.setNewUsername = setNewUsername;
+    }
+
+    public String getNewUsername() {
+        return newUsername;
+    }
+
+    public void setNewUsername(String newUsername) {
+        this.newUsername = newUsername;
+    }
+
+    public boolean isSetNewName() {
+        return setNewName;
+    }
+
+    public void setSetNewName(boolean setNewName) {
+        this.setNewName = setNewName;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+
+    public boolean isSetNewEmail() {
+        return setNewEmail;
+    }
+
+    public void setSetNewEmail(boolean setNewEmail) {
+        this.setNewEmail = setNewEmail;
+    }
+
+    public String getNewEmail() {
+        return newEmail;
+    }
+
+    public void setNewEmail(String newEmail) {
+        this.newEmail = newEmail;
+    }
+
+    public boolean isSetNewPassword() {
+        return setNewPassword;
+    }
+
+    public void setSetNewPassword(boolean setNewPassword) {
+        this.setNewPassword = setNewPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getNewPasswordRepeat() {
+        return newPasswordRepeat;
+    }
+
+    public void setNewPasswordRepeat(String newPasswordRepeat) {
+        this.newPasswordRepeat = newPasswordRepeat;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
