0377980a382256bbdb7f148badac0d96c150644a
Sebastian Sokolowski <sebo366@gmail.com>
2017-10-14 23:33:51 +0200

    Implement auto redirect authenticated user to week page.

--- src/main/java/bj/pranie/controller/IndexController.java
+++ src/main/java/bj/pranie/controller/IndexController.java
@@ -1,15 +1,11 @@
 package bj.pranie.controller;
 
-import bj.pranie.util.TimeUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import bj.pranie.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Calendar;
 
 /**
  * Created by Sebastian Sokolowski on 12.10.16.
@@ -17,14 +13,20 @@ import java.util.Calendar;
 
 @Controller
 public class IndexController {
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index(Model model) {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("time", TimeUtil.getTime());
-        log.debug("time" + Calendar.getInstance().getTime().toString());
+    public String index() {
+        if (isAuthenticatedUser()) {
+            return "redirect:/week";
+        }
+        return "index";
+    }
 
-        return modelAndView;
+    private boolean isAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof User) {
+            return true;
+        }
+        return false;
     }
 }
