f041bdd922dd9ebf249985ad6422fb6ec4462e53
Sebastian Sokolowski <sebo366@gmail.com>
2017-11-04 20:59:35 +0100

    Fix check wm without login.

--- src/main/java/bj/pranie/config/WebSecurityConfig.java
+++ src/main/java/bj/pranie/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/",
                         "/logout",
                         "/week", "/week/*",
-                        "/wm/*/*/*/*",
+                        "/wm/*/*/*/*/",
                         "/user/regulations", "/user/registration", "/user/restorePassword", "/user/resetPassword",
                         "/images/*",
                         "/favicon.ico",
