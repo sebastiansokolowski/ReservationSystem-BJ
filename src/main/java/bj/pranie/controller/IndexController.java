410ee6eae298ec72614bc9f868f458cdb9f3b280
Sebastian Sokolowski <sebo366@gmail.com>
2018-06-10 01:28:45 +0200

    Fix null pointer.

--- src/main/java/bj/pranie/controller/IndexController.java
+++ src/main/java/bj/pranie/controller/IndexController.java
@@ -1,11 +1,8 @@
 package bj.pranie.controller;
 
-import bj.pranie.Application;
-import bj.pranie.model.UserSettingsModel;
 import bj.pranie.service.UserAuthenticatedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,8 +17,7 @@ public class IndexController {
     private UserAuthenticatedService userAuthenticatedService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("holidays", Application.HOLIDAYS);
+    public String index() {
         if (userAuthenticatedService.isAuthenticatedUser()) {
             return "redirect:/week";
         }
