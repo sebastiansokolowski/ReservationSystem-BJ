c06d69566294afabde783a491ac41f4a2dfabb61
Sebastian Sokolowski <sebo366@gmail.com>
2018-08-12 14:40:03 +0200

    Remove unused import.

--- src/main/java/bj/pranie/controller/week/AdminWeekController.java
+++ src/main/java/bj/pranie/controller/week/AdminWeekController.java
@@ -7,7 +7,6 @@ import bj.pranie.entity.WashTime;
 import bj.pranie.entity.myEnum.ReservationType;
 import bj.pranie.service.UserAuthenticatedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
