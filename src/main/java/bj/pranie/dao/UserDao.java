4160632f676bcc619f34cab9f153337cfda9c051
Sebastian Sokolowski <sebo366@gmail.com>
2022-04-20 20:22:49 +0200

    Add some finding methods to UserDao and WashTimeDao

--- src/main/java/bj/pranie/dao/UserDao.java
+++ src/main/java/bj/pranie/dao/UserDao.java
@@ -2,6 +2,7 @@ package bj.pranie.dao;
 
 import bj.pranie.entity.Room;
 import bj.pranie.entity.User;
+import bj.pranie.entity.myEnum.UserRole;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,8 @@ public interface UserDao extends CrudRepository<User, Long> {
     User findByUsername(String username);
 
     List<User> findByRoom(Room room);
+
+    List<User> findByRole(UserRole role);
 
     User findByResetPasswordKey(String resetPasswordKey);
 
