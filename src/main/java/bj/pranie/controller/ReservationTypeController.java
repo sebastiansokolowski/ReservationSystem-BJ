d386beb581e19118992843ddeee28eb7810ce38e
sebastiansokolowski <sebo366@gmail.com>
2026-09-22 09:41:46 +0200

    Update reservation rules summary

--- src/main/java/bj/pranie/controller/ReservationTypeController.java
+++ src/main/java/bj/pranie/controller/ReservationTypeController.java
@@ -2,6 +2,7 @@ package bj.pranie.controller;
 
 import bj.pranie.service.UserAuthenticatedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,17 @@ public class ReservationTypeController {
     @Autowired
     private UserAuthenticatedService userAuthenticatedService;
 
+    @Value("${tokensPerWeek}")
+    private int tokensPerWeek;
+
+    @Value("${resetTime}")
+    private int resetTime;
+
     @RequestMapping(value = "/type", method = RequestMethod.GET)
     public String userSettings(Model model) {
         model.addAttribute("user", userAuthenticatedService.getAuthenticatedUser());
+        model.addAttribute("tokensPerWeek", tokensPerWeek);
+        model.addAttribute("resetTime", String.format("%02d:00", resetTime));
         return "type";
     }
 }
